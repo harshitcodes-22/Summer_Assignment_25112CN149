4ee5703baee870c2ada35be59f9fa2b22549d878
Harshit Giri <giriharshit22@gmail.com>
2026-05-29 22:47:55 +0530

    Added Day_1 and Day_2 structure

---
+++ Summer_Assignment_25112CN149/Day_1/MultiplicationTable.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    printf("The multiplication table of %d:\n",n);
+
+    for(int i=1;i<=10;i++){
+    
+        printf(" %d*%d=%d\n ",n,i,n*i);
+    }
+    return 0;
+}
