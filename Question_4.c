d3fc6384efea624791fd90171b2f2d5fad1958e6
Harshit Giri <giriharshit22@gmail.com>
2026-06-01 19:37:43 +0530

    updated

--- Question_4.c
+++ Question_4.c
@@ -1,4 +1,19 @@
 #include<stdio.h>
 int main(){
-    int n
+    int n,count=0;
+    printf("Enter the number:");
+    scanf("%d",&n);
+
+    if (n==0){
+        count=1;
+    }
+    else{
+        while(n!=0){
+            n=n/10;
+            count++;
+
+        }
+    }
+    printf("Number of digits=%d\n",count);
+    return 0;
 }
