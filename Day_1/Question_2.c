9bde9626d1124ab41af018ada1511eb464e14845
Harshit Giri <giriharshit22@gmail.com>
2026-06-01 21:01:04 +0530

    updated

--- Day_1/Question_2.c
+++ Day_1/Question_2.c
@@ -5,7 +5,7 @@ int main(){
     scanf("%d",&n);
     for (int i=1;i<=10;i++){
       Table=n*i;
-      printf(" %d*%d=%d\n",n,i,Table);
+      printf(" %d * %d = %d\n",n,i,Table);
     }
     return 0;
   }
